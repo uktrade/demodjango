309caaa0ff98161035692b2284e5febc5fe0c366
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2023-09-15 14:53:21 +0100

    Green if success else red

--- app/util.py
+++ app/util.py
@@ -1,5 +1,6 @@
 def render_connection_info(addon_type: str, success: bool, connection_info: str = False):
     output = (f'{addon_type}: '
-              f'<span style="color: green">{"✓" if success else "✗"}</span> <br> '
+              f'<span style="color: {"green" if success else "red"}">'
+              f'{"✓" if success else "✗"}</span> <br> '
               f'{f"<pre><code>{connection_info}</code></pre>" if connection_info else ""} <hr>')
     return output
