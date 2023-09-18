0572eca157aa88254b1b57b87a1b5bc3597f0fec
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2023-09-18 12:29:24 +0100

    Add sample email column and docker-compose for testing

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.9
+
+RUN apt-get update && apt-get upgrade -y
+RUN mkdir /app
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+ENTRYPOINT ["gunicorn", "-b", "0.0.0.0:8080", "demodjango.wsgi:application"]
--- app/models.py
+++ app/models.py
@@ -6,4 +6,4 @@ from django.db import models
 class SampleTable(models.Model):
     sampleid = models.CharField(max_length=90)
     sample_name = models.CharField(max_length=60)
-    sample_email = models.EmailField(max_length=256)
+    sample_email = models.EmailField(max_length=256, null=True)
