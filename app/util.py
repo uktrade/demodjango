820219dbb314049f55ea0bd1b36bc1f0dc9341d1
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-11-24 16:41:00 +0000

    mostly commenting out stuff that results in errors locally

--- app/util.py
+++ app/util.py
@@ -2,7 +2,7 @@ import logging
 
 
 def render_connection_info(addon_type: str, success: bool, connection_info: str = False):
-    logging.getLogger("django.request").info(f"Rendering {addon_type} stuff")
+    logging.getLogger("django").info(f"Rendering {addon_type} stuff")
     output = (f'{addon_type}: '
               f'<span style="color: {"green" if success else "red"}">'
               f'{"✓" if success else "✗"}</span> <br> '
--- app/views.py
+++ app/views.py
@@ -11,49 +11,52 @@ from elasticsearch import Elasticsearch
 
 
 def index(request):
+    logging.getLogger("django.request").info("Rendering landing page")
+
     status_output = render_connection_info('Server Time', True, str(datetime.now()))
 
-    try:
-        with connections['rds'].cursor() as c:
-            c.execute('SELECT version()')
-            status_output += render_connection_info('PostgreSQL (RDS)', True, c.fetchone()[0])
-    except Exception as e:
-        status_output += render_connection_info('PostgreSQL (RDS)', False, str(e))
-
-    try:
-        with connections['aurora'].cursor() as c:
-            c.execute('SELECT version()')
-            status_output += render_connection_info('PostgreSQL (Aurora)', True, c.fetchone()[0])
-    except Exception as e:
-        status_output += render_connection_info('PostgreSQL (Aurora)', False, str(e))
-
-    try:
-        with connections['default'].cursor() as c:
-            c.execute('SELECT SQLITE_VERSION()')
-            status_output += render_connection_info('SQLite3', True, c.fetchone()[0])
-    except Exception as e:
-        status_output += render_connection_info('SQLite3', False, str(e))
-
-    try:
-        r = redis.Redis.from_url(f'{settings.REDIS_ENDPOINT}/0')
-        status_output += render_connection_info('Redis', True, r.get('Using').decode())
-    except Exception as e:
-        status_output += render_connection_info('Redis', False, str(e))
-
-    try:
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket(settings.S3_BUCKET_NAME)
-        body = bucket.Object('sample_file.txt')
-        status_output += render_connection_info('S3 Bucket', True, body.get()['Body'].read().decode())
-    except Exception as e:
-        status_output += render_connection_info('S3 Bucket', False, str(e))
-
-    try:
-        es = Elasticsearch(f'{settings.OPENSEARCH_ENDPOINT}')
-        res = es.get(index="test-index", id=1)
-        status_output += render_connection_info('OpenSearch', True, res['_source']['text'])
-    except Exception as e:
-        status_output += render_connection_info('OpenSearch', False, str(e))
+    # Todo: Temporarily commented out for less noise when running locally...
+    # try:
+    #     with connections['rds'].cursor() as c:
+    #         c.execute('SELECT version()')
+    #         status_output += render_connection_info('PostgreSQL (RDS)', True, c.fetchone()[0])
+    # except Exception as e:
+    #     status_output += render_connection_info('PostgreSQL (RDS)', False, str(e))
+    #
+    # try:
+    #     with connections['aurora'].cursor() as c:
+    #         c.execute('SELECT version()')
+    #         status_output += render_connection_info('PostgreSQL (Aurora)', True, c.fetchone()[0])
+    # except Exception as e:
+    #     status_output += render_connection_info('PostgreSQL (Aurora)', False, str(e))
+    #
+    # try:
+    #     with connections['default'].cursor() as c:
+    #         c.execute('SELECT SQLITE_VERSION()')
+    #         status_output += render_connection_info('SQLite3', True, c.fetchone()[0])
+    # except Exception as e:
+    #     status_output += render_connection_info('SQLite3', False, str(e))
+    #
+    # try:
+    #     r = redis.Redis.from_url(f'{settings.REDIS_ENDPOINT}/0')
+    #     status_output += render_connection_info('Redis', True, r.get('Using').decode())
+    # except Exception as e:
+    #     status_output += render_connection_info('Redis', False, str(e))
+    #
+    # try:
+    #     s3 = boto3.resource('s3')
+    #     bucket = s3.Bucket(settings.S3_BUCKET_NAME)
+    #     body = bucket.Object('sample_file.txt')
+    #     status_output += render_connection_info('S3 Bucket', True, body.get()['Body'].read().decode())
+    # except Exception as e:
+    #     status_output += render_connection_info('S3 Bucket', False, str(e))
+    #
+    # try:
+    #     es = Elasticsearch(f'{settings.OPENSEARCH_ENDPOINT}')
+    #     res = es.get(index="test-index", id=1)
+    #     status_output += render_connection_info('OpenSearch', True, res['_source']['text'])
+    # except Exception as e:
+    #     status_output += render_connection_info('OpenSearch', False, str(e))
 
     return HttpResponse("<!doctype html><html><head><title>DemoDjango</title></head><body>"
                         f"{status_output}"
