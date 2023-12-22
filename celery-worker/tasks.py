7f7a191c095c3e3efe4f18389b59aa1f845d8562
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-12-22 06:48:32 +0000

    Tidy up celery-worker/tasks.py

--- celery-worker/tasks.py
+++ celery-worker/tasks.py
@@ -1,8 +1,6 @@
 from config.celery import celery_app
-from feedback import utils
-from peoplefinder.services.uk_staff_locations import UkStaffLocationService
 
 
 @celery_app.task(bind=True)
 def debug_task(self):
-    print("Request: {0!r}".format(self.request))  # noqa
+    print("Request: {0!r}".format(self.request))
