956b8401e1a48b4e831c2e257e5d86518672c971
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-01-02 15:40:46 +0000

    Change DjangoCelery to demodjango_celery

--- config/celery.py
+++ config/celery.py
@@ -4,6 +4,6 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-celery_app = Celery("DjangoCelery")
+celery_app = Celery("demodjango_celery")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
