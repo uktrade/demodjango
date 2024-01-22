f62e0853a2fa1e417f3f916ec8b623d72c2d3676
Yusuf <47318342+yusufsheiqh@users.noreply.github.com>
2024-01-22 10:07:56 +0000

    DBTP-660 - Add OpenTelemetry configuration for AWS X-Ray (#42)
    Co-authored-by: Lawrence <34475808+acodeninja@users.noreply.github.com>

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: python manage.py migrate && python manage.py migrate --database rds && python manage.py migrate --database aurora && python manage.py load_defaults && gunicorn -b 0.0.0.0:$PORT demodjango.wsgi:application
+web: python manage.py migrate && python manage.py migrate --database rds && python manage.py migrate --database aurora && python manage.py load_defaults && opentelemetry-instrument gunicorn -b 0.0.0.0:$PORT demodjango.wsgi:application
 celery-worker: celery --app demodjango.celery worker --task-events --loglevel INFO
 check: python manage.py check
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 dockerize -wait tcp://opensearch:9200 -timeout 60s
 
-python manage.py migrate && python manage.py migrate --database rds && python manage.py migrate --database aurora && python manage.py load_defaults && gunicorn -b 0.0.0.0:8080 demodjango.wsgi:application
+python manage.py migrate
+python manage.py migrate --database rds
+python manage.py migrate --database aurora
+python manage.py load_defaults
+
+OTEL_PROPAGATORS=xray OTEL_PYTHON_ID_GENERATOR=xray OTEL_SERVICE_NAME=your-service-name OTEL_TRACES_EXPORTER=console,otlp OTEL_METRICS_EXPORTER=console,otlp opentelemetry-instrument gunicorn -b 0.0.0.0:8080 demodjango.wsgi:application
