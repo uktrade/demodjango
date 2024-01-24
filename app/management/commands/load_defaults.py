f340e8f2349dba4023ecda64b4cfb388b007e6af
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-01-24 12:20:18 +0000

    Hack it about to run web service outside Docker Compose

--- app/management/commands/load_defaults.py
+++ app/management/commands/load_defaults.py
@@ -16,25 +16,25 @@ class Command(BaseCommand):
 
         SampleTable.objects.update_or_create(sampleid=1, sample_name="Database is connected")
 
-        if settings.REDIS_ENDPOINT:
-            r = redis.Redis.from_url(f'{settings.REDIS_ENDPOINT}')
-            r.set('test-data', 'Test content read from Redis')
-
-        if settings.S3_BUCKET_NAME:
-            object_name = os.path.basename("sample_file.txt")
-            s3_client = boto3.client('s3')
-            try:
-                s3_client.upload_file(object_name, settings.S3_BUCKET_NAME, object_name)
-            except ClientError as e:
-                logging.error(e)
-
-        if settings.OPENSEARCH_ENDPOINT:
-            opensearch_client = OpenSearch(f'{settings.OPENSEARCH_ENDPOINT}')
-
-            doc = {
-                'author': 'author_name',
-                'text': 'Test content read from OpenSearch.',
-                'timestamp': datetime.now(),
-            }
-            response = opensearch_client.index(index="test-index", id=1, body=doc)
-            print(response['result'])
+        # if settings.REDIS_ENDPOINT:
+        #     r = redis.Redis.from_url(f'{settings.REDIS_ENDPOINT}')
+        #     r.set('test-data', 'Test content read from Redis')
+        #
+        # if settings.S3_BUCKET_NAME:
+        #     object_name = os.path.basename("sample_file.txt")
+        #     s3_client = boto3.client('s3')
+        #     try:
+        #         s3_client.upload_file(object_name, settings.S3_BUCKET_NAME, object_name)
+        #     except ClientError as e:
+        #         logging.error(e)
+        #
+        # if settings.OPENSEARCH_ENDPOINT:
+        #     opensearch_client = OpenSearch(f'{settings.OPENSEARCH_ENDPOINT}')
+        #
+        #     doc = {
+        #         'author': 'author_name',
+        #         'text': 'Test content read from OpenSearch.',
+        #         'timestamp': datetime.now(),
+        #     }
+        #     response = opensearch_client.index(index="test-index", id=1, body=doc)
+        #     print(response['result'])
