9ac283451f2578a329512ed1e8f5350a9338095e
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2024-02-14 12:46:08 +0000

    feat: add a http healthcheck with a default call to httpstat.us

--- app/views.py
+++ app/views.py
@@ -3,9 +3,11 @@ import logging
 import os
 from datetime import datetime
 from typing import Dict, Callable
+from urllib.parse import urlparse
 
 import boto3
 import redis
+import requests
 from django.conf import settings
 from django.db import connections
 from django.http import HttpResponse
@@ -13,7 +15,7 @@ from opensearchpy import OpenSearch
 from tenacity import retry, stop_after_delay, RetryError, wait_fixed
 
 from celery_worker.tasks import demodjango_task
-from .util import render_connection_info
+from .util import render_connection_info, STATUS_SUCCESS, STATUS_FAIL
 
 logger = logging.getLogger("django")
 
@@ -26,6 +28,7 @@ REDIS = 'redis'
 S3 = 's3'
 SERVER_TIME = 'server_time'
 SQLITE = 'sqlite3'
+HTTP_CONNECTION = 'http'
 
 ALL_CHECKS = {
     CELERY: 'Celery Worker',
@@ -37,6 +40,7 @@ ALL_CHECKS = {
     S3: 'S3 Bucket',
     SERVER_TIME: 'Server Time',
     SQLITE: 'SQLite3',
+    HTTP_CONNECTION: 'HTTP Checks',
 }
 
 
@@ -59,6 +63,7 @@ def index(request):
         S3: s3_bucket_check,
         OPENSEARCH: opensearch_check,
         CELERY: celery_worker_check,
+        HTTP_CONNECTION: http_check,
     }
 
     if settings.ACTIVE_CHECKS:
@@ -195,3 +200,51 @@ def git_information():
     return render_connection_info(ALL_CHECKS[GIT_INFORMATION],
                                   git_commit != "Unknown",
                                   f"Commit: {git_commit}, Branch: {git_branch}, Tag: {git_tag}")
+
+
+def http_check():
+    urls = os.environ.get("HTTP_CHECK_URLS", "https://httpstat.us/200|200|GET").split(",")
+    urls = [u for u in urls if u]
+
+    passed = True
+    message = ""
+
+    if not urls:
+        passed = False
+        message = ("No urls provided, set HTTP_CHECK_URLS to a comma separated list of "
+                   "URL|STATUS_CODE|METHOD Example: 'http://api.dev.demodjango.internal|200|GET,"
+                   "http://api.dev.demodjango.internal/path|403|POST'")
+
+    for url in urls:
+        segments = url.split("|")
+        url = segments[0]
+        url_parsed = urlparse(url)
+        status = int(segments[1]) if len(segments) > 1 else 200
+        method = segments[2].lower() if len(segments) > 2 else 'get'
+        request_success = True
+
+        check_message = f"{method.upper()} {url_parsed.hostname}{url_parsed.path} "
+
+        requester = getattr(requests, method, None)
+        if not requester:
+            check_message += f"invalid HTTP method {method} "
+            request_success = False
+
+        if request_success:
+            try:
+                r = requester(url)
+
+                if r.status_code != status:
+                    check_message += f"response has status code <code>{r.status_code}</code>, but expected <code>{status}</code> "
+                    passed = False
+                    request_success = False
+            except Exception as e:
+                passed = False
+                request_success = False
+                check_message += f"{e} "
+
+        check_message = f'<span style="color: green">{STATUS_SUCCESS}</span> {check_message}' if request_success else f'<span style="color: red">{STATUS_FAIL}</span> {check_message}'
+        check_message += "<br>"
+        message += check_message
+
+    return render_connection_info(ALL_CHECKS[HTTP_CONNECTION], passed, message)
