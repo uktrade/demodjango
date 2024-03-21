de815b3fd6494a5927d8babfe55fb7921792ed51
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-03-21 14:09:29 +0000

    add unique together constraint on sample_id and sample_name

--- app/models.py
+++ app/models.py
@@ -1,12 +1,16 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 
 
 class SampleTable(models.Model):
-    sampleid = models.CharField(max_length=90)
+    sample_id = models.CharField(max_length=90)
     sample_name = models.CharField(max_length=60)
     sample_email = models.EmailField(max_length=256, null=True)
+    
+    class Meta:
+        unique_together = ('sample_id', 'sample_name',)
 
 
 class ScheduledTask(models.Model):
