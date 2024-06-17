58e9049bc112bb5269458b3b586416b964bab045
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-06-17 11:25:13 +0100

    agg playwright command to smoke test script

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -11,7 +11,10 @@ case "${target}" in
     ;;
 esac
 
-echo "Running smoke tests against ${host}"
+echo -e "\nInstalling required browsers"
+poetry run playwright install
+
+echo -e "\nRunning smoke tests against ${host}"
 
 export LANDING_PAGE_URL=${host}
 poetry run pytest ./tests/smoke
