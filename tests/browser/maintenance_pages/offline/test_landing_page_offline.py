06cdffbc7ac60ff4bcfea3ff7a96a3fb299220d4
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-03 16:55:57 +0100

    add 503 assertion and potential bypass value test

--- tests/browser/maintenance_pages/offline/test_landing_page_offline.py
+++ tests/browser/maintenance_pages/offline/test_landing_page_offline.py
@@ -9,12 +9,16 @@ from tests.browser.src.landing_page_checks import assert_landing_page_has_normal
 def test_page_loads_ok_and_shows_default_offline_message(page: Page):
     response = page.goto(os.getenv("LANDING_PAGE_URL"))
 
-    assert response.status == HTTPStatus.OK
+    assert response.status == 503
 
-    expect(page.locator('body')).to_have_text("put default message here")
+    expect(page.locator('body')).to_have_text("The service is currently unavailable. Don't worry, we'll be back soon.")
 
 
 def test_with_bypass_header_page_loads_ok_with_normal_content(page: Page):
+    BYPASS_VALUE = os.getenv("BYPASS_VALUE")
+    page.context.route("**/*", lambda route, request: route.continue_(
+        headers={**request.headers, "BypassValue": BYPASS_VALUE}
+    ))
     response = page.goto(os.getenv("LANDING_PAGE_URL"))
 
     assert response.status == HTTPStatus.OK
