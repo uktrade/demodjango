d11100f5bc7093b47082a17e07de363a0cbed0ba
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-04 09:52:27 +0100

    get tests passing

--- tests/browser/maintenance_pages/offline/test_landing_page_offline.py
+++ tests/browser/maintenance_pages/offline/test_landing_page_offline.py
@@ -15,14 +15,15 @@ def test_page_loads_ok_and_shows_default_offline_message(page: Page):
 
 
 def test_with_bypass_header_page_loads_ok_with_normal_content(page: Page):
-    BYPASS_VALUE = os.getenv("BYPASS_VALUE")
+    # BYPASS_VALUE = os.getenv("BYPASS_VALUE")
+    BYPASS_VALUE = "abcdefg"
     page.context.route("**/*", lambda route, request: route.continue_(
-        headers={**request.headers, "BypassValue": BYPASS_VALUE}
+        headers={**request.headers, "Bypass-Key": BYPASS_VALUE}
     ))
+    
     response = page.goto(os.getenv("LANDING_PAGE_URL"))
 
-    assert response.status == HTTPStatus.OK
-
+    assert response.status == 200
     assert_landing_page_has_normal_content(page)
 
 # Todo: check env ip is whitelisted, is this a browser test or actually a curl etc. job?
