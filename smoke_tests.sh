9b10a1f6448e056cf8fda5c76ff7513fd56baae8
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-07-08 09:33:13 +0100

    Update smoke_tests.sh

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Todo: Remove this once platform-tols regression tests etc. have been adjusted to use the new browser tests script
+# Todo: Remove this once platform-tools regression tests etc. have been adjusted to use the new browser tests script
 
 ./tests/browser/run.sh "$1" "smoke"
