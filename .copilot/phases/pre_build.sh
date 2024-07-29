655b47602f567684561950f3412a5a8015840dc6
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-29 12:54:17 +0100

    use sed command

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -9,19 +9,19 @@ set -e
 
 # poetry run pytest tests/app
 
-# GITMODULES_FILE="./.gitmodules"
+GITMODULES_FILE="./.gitmodules"
 
-# sed -i 's|git@github.com:\(.*\)|https://github.com/\1|g' "$GITMODULES_FILE"
+sed -i 's|git@github.com:\(.*\)|https://github.com/\1|g' "$GITMODULES_FILE"
 
-git_clone_base_url="https://codestar-connections.eu-west-2.amazonaws.com/git-http/563763463626/eu-west-2/122da7c9-9fa3-42b2-9125-1db0f957913d/uktrade"
+# git_clone_base_url="https://codestar-connections.eu-west-2.amazonaws.com/git-http/563763463626/eu-west-2/122da7c9-9fa3-42b2-9125-1db0f957913d/uktrade"
 
-git config --global credential.helper '!aws codecommit credential-helper $@'
-git config --global credential.UseHttpPath true
+# git config --global credential.helper '!aws codecommit credential-helper $@'
+# git config --global credential.UseHttpPath true
 
-cat <<EOF > ./.gitmodules
-[submodule "demodjango-private"]
-	path = demodjango-private
-	url = $git_clone_base_url/demodjango-private.git
-EOF
+# cat <<EOF > ./.gitmodules
+# [submodule "demodjango-private"]
+# 	path = demodjango-private
+# 	url = $git_clone_base_url/demodjango-private.git
+# EOF
 
 git submodule update --init --remote --recursive
