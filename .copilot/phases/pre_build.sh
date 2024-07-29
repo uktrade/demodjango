771f89928c8d5fb04d30c3d5aefa62d5abcbb29f
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-29 13:25:18 +0100

    update pre_build with sed script

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -8,10 +8,15 @@ set -e
 # Add commands below to run as part of the pre_build phase
 
 # poetry run pytest tests/app
+if [ -f "./.gitmodules" ]; then
+  echo ".gitmodules file exists. Modifying URLs..."
+    account_id=$(echo $CODESTAR_CONNECTION_ARN | cut -d':' -f5)
+    connection_id=$(echo $CODESTAR_CONNECTION_ARN | cut -d'/' -f2)
+    git_clone_base_url="https://codestar-connections.eu-west-2.amazonaws.com/git-http/$account_id/eu-west-2/$connection_id/uktrade"
 
-GITMODULES_FILE="./.gitmodules"
+    # GITMODULES_FILE="./.gitmodules"
 
-sed -i 's|git@github.com:\(.*\)|https://github.com/\1|g' "$GITMODULES_FILE"
+    sed -i "s|url = git@github.com:uktrade/\(.*\).git|url = $git_clone_base_url/\1.git|g" ./.gitmodules
 
 # git_clone_base_url="https://codestar-connections.eu-west-2.amazonaws.com/git-http/563763463626/eu-west-2/122da7c9-9fa3-42b2-9125-1db0f957913d/uktrade"
 
@@ -24,4 +29,8 @@ sed -i 's|git@github.com:\(.*\)|https://github.com/\1|g' "$GITMODULES_FILE"
 # 	url = $git_clone_base_url/demodjango-private.git
 # EOF
 
-git submodule update --init --remote --recursive
+    git submodule update --init --remote --recursive
+
+else
+  echo ".gitmodules file does not exist. No URLs to update."
+fi
