dc6671dff7451f25f183464ed36eb39c40f7757f
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-29 14:57:44 +0100

    hardcode arn and add git config

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -6,13 +6,16 @@ set -e
 # git config --global credential.UseHttpPath true
 
 # Add commands below to run as part of the pre_build phase
-
+CODESTAR_CONNECTION_ARN="arn:aws:codestar-connections:eu-west-2:563763463626:connection/122da7c9-9fa3-42b2-9125-1db0f957913d"
 # poetry run pytest tests/app
 if [ -f "./.gitmodules" ]; then
   echo ".gitmodules file exists. Modifying URLs..."
     account_id=$(echo $CODESTAR_CONNECTION_ARN | cut -d':' -f5)
     connection_id=$(echo $CODESTAR_CONNECTION_ARN | cut -d'/' -f2)
     git_clone_base_url="https://codestar-connections.eu-west-2.amazonaws.com/git-http/$account_id/eu-west-2/$connection_id/uktrade"
+
+    git config --global credential.helper '!aws codecommit credential-helper $@'
+    git config --global credential.UseHttpPath true
 
     # GITMODULES_FILE="./.gitmodules"
 
