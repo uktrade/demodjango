39e57f8923d11825cb960b40d68f14d84e8741d1
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-29 16:22:33 +0100

    run codebase prepare

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -3,11 +3,6 @@
 # Exit early if something goes wrong
 set -e
 
-# git config --global credential.UseHttpPath true
-
-# Add commands below to run as part of the pre_build phase
-
-# poetry run pytest tests/app
 if [ -f "./.gitmodules" ]; then
   echo ".gitmodules file exists. Modifying URLs..."
     account_id=$(echo $CODESTAR_CONNECTION_ARN | cut -d':' -f5)
@@ -17,23 +12,12 @@ if [ -f "./.gitmodules" ]; then
     git config --global credential.helper '!aws codecommit credential-helper $@'
     git config --global credential.UseHttpPath true
 
-    # GITMODULES_FILE="./.gitmodules"
-
     sed -i "s|url = git@github.com:uktrade/\(.*\).git|url = $git_clone_base_url/\1.git|g" ./.gitmodules
-
-# git_clone_base_url="https://codestar-connections.eu-west-2.amazonaws.com/git-http/563763463626/eu-west-2/122da7c9-9fa3-42b2-9125-1db0f957913d/uktrade"
-
-# git config --global credential.helper '!aws codecommit credential-helper $@'
-# git config --global credential.UseHttpPath true
-
-# cat <<EOF > ./.gitmodules
-# [submodule "demodjango-private"]
-# 	path = demodjango-private
-# 	url = $git_clone_base_url/demodjango-private.git
-# EOF
 
     git submodule update --init --remote --recursive
 
 else
   echo ".gitmodules file does not exist. No URLs to update."
 fi
+
+# Add commands below to run as part of the pre_build phase
