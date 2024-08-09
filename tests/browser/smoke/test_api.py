c259c7b000e3f41875067f5ca73857ff3a6488e9
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-08-09 14:47:21 +0100

    remove pytest unused

--- tests/browser/smoke/test_api.py
+++ tests/browser/smoke/test_api.py
@@ -1,44 +1,45 @@
-import json
 import os
 from datetime import datetime
 from datetime import timedelta
-from unittest.mock import patch
 
-import pytest
 from playwright.sync_api import Page
+
 
 def is_approx_now(time: datetime):
     now = datetime.now()
     tolerance = timedelta(seconds=60)
-    
+
     return abs(now - time) <= tolerance
+
 
 def test_api_response(page: Page):
     landing_page_url = os.getenv("LANDING_PAGE_URL")
     api_url = landing_page_url.replace("https://internal.", "https://internal.api.")
-    
+
     response = page.goto(api_url)
-    
+
     assert response.status == 200
-    
+
     response_data = response.json()
-    
-    assert response_data['message'] == "Success"
-    
-    timestamp_str = response_data['timestamp']
+
+    assert response_data["message"] == "Success"
+
+    timestamp_str = response_data["timestamp"]
     time = datetime.fromisoformat(timestamp_str)
-    
+
     assert is_approx_now(time)
 
+
 if os.getenv("IS_API"):
+
     def test_web_response(page: Page):
         landing_page_url = os.getenv("LANDING_PAGE_URL")
         api_url = landing_page_url.replace("https://internal.", "https://internal.api.")
-        
+
         response = page.goto(f"{api_url}test-web")
-        
+
         assert response.status == 200
-        
+
         response_data = response.json()
-    
-        assert response_data['message'] == "API reached web service"
+
+        assert response_data["message"] == "API reached web service"
