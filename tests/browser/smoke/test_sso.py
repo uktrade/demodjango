8c0f51c5e0aac39ade668aa2165a66dbc24073c1
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2024-09-12 20:04:45 +0100

    unwanted cdn config

--- tests/browser/smoke/test_sso.py
+++ tests/browser/smoke/test_sso.py
@@ -5,9 +5,6 @@ from playwright.sync_api import Page
 
 def test_sso_flow(page: Page):
     landing_page_url = os.getenv("LANDING_PAGE_URL")
-    IS_CDN = os.getenv("IS_CDN")
-    if IS_CDN:
-        landing_page_url = landing_page_url.replace("https://", "https://internal.")
     full_url = f"{landing_page_url}sso"
 
     response = page.goto(full_url)
