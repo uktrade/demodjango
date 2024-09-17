52200e4a1c715cf5af75780edaaedef1005830c5
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2024-09-17 11:15:37 +0100

    feat: Add public domain endpoint to demodjango (#102)

--- tests/browser/smoke/test_landing_page.py
+++ tests/browser/smoke/test_landing_page.py
@@ -3,12 +3,14 @@ from http import HTTPStatus
 
 from playwright.sync_api import Page
 
-from tests.browser.src.landing_page_checks import assert_landing_page_has_normal_content
+from tests.browser.src.landing_page_checks import assert_landing_page_has_normal_content, assert_landing_page_has_normal_content_cdn
 
 
 def test_page_loads_with_title_and_has_success_ticks(page: Page):
     response = page.goto(os.getenv("LANDING_PAGE_URL"))
 
     assert response.status == HTTPStatus.OK
-
-    assert_landing_page_has_normal_content(page)
+    if os.getenv("IS_CDN"):
+        assert_landing_page_has_normal_content_cdn(page)
+    else:
+        assert_landing_page_has_normal_content(page)
