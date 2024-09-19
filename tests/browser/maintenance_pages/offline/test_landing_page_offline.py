3b0cacdebadd48ac457e2b76cdfa9e12fc2273af
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-09-19 14:18:44 +0100

    restore cdn logic

--- tests/browser/maintenance_pages/offline/test_landing_page_offline.py
+++ tests/browser/maintenance_pages/offline/test_landing_page_offline.py
@@ -4,6 +4,9 @@ from playwright.sync_api import Page
 from playwright.sync_api import expect
 
 from tests.browser.src.landing_page_checks import assert_landing_page_has_normal_content
+from tests.browser.src.landing_page_checks import (
+    assert_landing_page_has_normal_content_cdn,
+)
 
 
 def test_page_loads_ok_and_shows_default_offline_message(page: Page):
@@ -28,7 +31,11 @@ def test_with_bypass_header_page_loads_ok_with_normal_content(page: Page):
     response = page.goto(os.getenv("LANDING_PAGE_URL"))
 
     assert response.status == 200
-    assert_landing_page_has_normal_content(page)
+
+    if os.getenv("IS_CDN"):
+        assert_landing_page_has_normal_content_cdn(page)
+    else:
+        assert_landing_page_has_normal_content(page)
 
 
 def test_frontend_to_api(page: Page):
