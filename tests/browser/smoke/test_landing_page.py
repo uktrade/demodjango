fedabc7ac940b7cf5a345fc8dac4aa11d5a674d1
Kate Sugden <kate.sugden@madetech.com>
2024-11-29 10:57:11 +0000

    Update name key to type

--- tests/browser/smoke/test_landing_page.py
+++ tests/browser/smoke/test_landing_page.py
@@ -5,7 +5,7 @@ from http import HTTPStatus
 
 
 def test_page_loads_with_title_and_has_success_ticks():
-    url = os.getenv("WEB_SERVICE_URL") + "?json=true"
+    url = os.getenv("WEB_SERVICE_URL") or "http://localhost:8080/" + "?json=true"
     with urllib.request.urlopen(url) as response:
         data = json.loads(response.read())
 
@@ -14,6 +14,6 @@ def test_page_loads_with_title_and_has_success_ticks():
         for result in results:
             check_success = "OK" if result["success"] else "FAIL"
             failure_message = "" if result["success"] else f" ({result['message']})"
-            print(f"{result['name']}: {check_success}{failure_message}")
+            print(f"{result['type']}: {check_success}{failure_message}")
 
         assert all([result["success"] for result in results])
