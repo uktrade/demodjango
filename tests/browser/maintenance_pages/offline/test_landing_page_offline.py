998d6f1c185bef5218511abc04d1d3a5249ee61e
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2025-02-28 09:13:55 +0000

    Maintenance page tests - use IP-Filter-Test service instead

--- tests/browser/maintenance_pages/offline/test_landing_page_offline.py
+++ tests/browser/maintenance_pages/offline/test_landing_page_offline.py
@@ -9,9 +9,9 @@ from tests.browser.src.landing_page_checks import assert_landing_page_has_normal
 
 
 def test_maintenance_page_loads_ok_and_shows_default_offline_message(page: Page):
-    response = page.goto(os.getenv("WEB_SERVICE_URL"))
+    response = page.goto(os.getenv("IP_FILTER_TEST_URL"))
 
-    # assert response.status == 503
+    assert response.status == 503
 
     expect(page.locator("body")).to_contain_text(
         "The service is currently unavailable. Don't worry, we'll be back soon."
@@ -27,7 +27,7 @@ def test_with_bypass_header_page_loads_ok_with_normal_content(page: Page):
         ),
     )
 
-    response = page.goto(os.getenv("WEB_SERVICE_URL"))
+    response = page.goto(os.getenv("IP_FILTER_TEST_URL"))
 
     assert response.status == 200
     assert_landing_page_has_normal_content(page)
@@ -41,8 +41,8 @@ def test_frontend_to_api(page: Page):
             headers={**request.headers, "Bypass-Key": MAINTENANCE_PAGE_BYPASS_VALUE}
         ),
     )
-    web_service_url = os.getenv("WEB_SERVICE_URL")
-    test_api_url = f"{web_service_url}test-api"
+    ip_filter_test_service_url = os.getenv("IP_FILTER_TEST_URL")
+    test_api_url = f"{ip_filter_test_service_url}test-api"
     expected_api_service_url = os.getenv("API_SERVICE_URL")
 
     response = page.goto(test_api_url)
