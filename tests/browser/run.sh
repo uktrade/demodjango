4ac70986d5368344bce6ae2364147a83589d1cc9
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2025-05-12 15:38:36 +0100

    Add urls for prod based environments

--- tests/browser/run.sh
+++ tests/browser/run.sh
@@ -2,12 +2,22 @@
 
 target="$1"
 tests="$2"
-maintenace_page_bypass_value="$3"
+maintenance_page_bypass_value="$3"
 
 case "${target}" in
   "" | "local")
     web_service_url="http://localhost:8080"
     # Todo: If we want to be able to test this locally via Docker Compose api_service_url="http://localhost:8080"
+     ;;
+  "prod")
+    web_service_url="https://web.demodjango.prod.uktrade.digital/"
+    api_service_url="https://api.demodjango.prod.uktrade.digital/"
+    ip_filter_test_service_url="https://ip-filter-test.demodjango.prod.uktrade.digital/"
+    ;;
+  "prode2e")
+    web_service_url="https://web.prode2e.demodjango.prod.uktrade.digital/"
+    api_service_url="https://api.prode2e.demodjango.prod.uktrade.digital/"
+    ip_filter_test_service_url="https://ip-filter-test.prode2e.demodjango.prod.uktrade.digital/"
     ;;
   *)
     web_service_url="https://web.${target}.demodjango.uktrade.digital/"
@@ -16,16 +26,16 @@ case "${target}" in
     ;;
 esac
 
-echo -e "\nInstall playwright browsers and dependencies"
+echo -e "\nInstalling Playwright browsers and dependencies..."
 poetry run playwright install --with-deps
 
-echo -e "\nRunning $tests tests against ${target} environment"
+echo -e "\nRunning $tests tests against the ${target} environment..."
 export WEB_SERVICE_URL=${web_service_url}
 export API_SERVICE_URL=${api_service_url}
 export IP_FILTER_TEST_SERVICE_URL=${ip_filter_test_service_url}
 
-if [ -n "$maintenace_page_bypass_value" ]; then
-  export MAINTENANCE_PAGE_BYPASS_VALUE=${maintenace_page_bypass_value}
+if [ -n "$maintenance_page_bypass_value" ]; then
+  export MAINTENANCE_PAGE_BYPASS_VALUE=${maintenance_page_bypass_value}
 fi
 
 poetry run pytest ./tests/browser/$tests
